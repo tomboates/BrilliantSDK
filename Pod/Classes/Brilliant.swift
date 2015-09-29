6ad54d06f9209a1c60b8ab16753c4060e841d60e
Paul Berry <pdbthefifth@gmail.com>
2015-09-29 19:08:05 -0400

    removed extra }, compiling now

--- Pod/Classes/Brilliant.swift
+++ Pod/Classes/Brilliant.swift
@@ -45,31 +45,30 @@ public class Brilliant {
   
   // send NPS Survey to Brilliant Server
   public func sendCompletedSurvey(var attributes:Dictionary<String, String>) {
-      // set headers for auth and JSON content-type
-      let headers = [
-        "X-App-Key": self.appKey!,
-        "Content-Type": "application/json"
-      ]
-      
-      // add user details to attributes
-      attributes["userEmail"] = self.userEmail
-      attributes["userAcctCreation"] = String(self.userAcctCreationDate)
-      attributes["userType"] = self.userType
-      
-      let params = ["nps_survey": attributes]
-      
-      // now send data
-      Alamofire.request(.POST, "\(kBaseURL)surveys", headers: headers, parameters: params, encoding: .JSON)
-        .responseString { _, _, result in
-          print("Success: \(result.isSuccess)")
-          print("Response String: \(result.value)")
-          
-      }
+    // set headers for auth and JSON content-type
+    let headers = [
+      "X-App-Key": self.appKey!,
+      "Content-Type": "application/json"
+    ]
+    
+    // add user details to attributes
+    attributes["userEmail"] = self.userEmail
+    attributes["userAcctCreation"] = String(self.userAcctCreationDate)
+    attributes["userType"] = self.userType
+    
+    let params = ["nps_survey": attributes]
+    
+    // now send data
+    Alamofire.request(.POST, "\(kBaseURL)surveys", headers: headers, parameters: params, encoding: .JSON)
+      .responseString { _, _, result in
+        print("Success: \(result.isSuccess)")
+        print("Response String: \(result.value)")
+        
     }
   }
-  
+
   //# MARK: - Helpers
-  
+
   private func daysSinceLastSurvey() -> Int {
     return NSCalendar.currentCalendar().components(.Day, fromDate: self.lastSurveyShownTime, toDate: NSDate(), options: []).day
   }
