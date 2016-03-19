638f48de5c5d99acf781bedc6ee21c9c1f587efa
Phillip Connaughton <phillip.connaughton@runkeeper.com>
2016-03-19 09:54:44 -0400

    Added keyboard toolbar done button

--- Pod/Classes/Brilliant.swift
+++ Pod/Classes/Brilliant.swift
@@ -202,7 +202,7 @@ public class Brilliant: NSObject {
             weakSelf?.appName = JSON["name"] as? String
             
             if let eligible = JSON["eligible"] as? Bool {
-                weakSelf?.eligible = true //eligible
+                weakSelf?.eligible = eligible
             }
             
             LogUtil.printDebug("initialization server call success. Setting app name to: \(self.appName)")
